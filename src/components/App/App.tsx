ca6be5ed313cd40793f726a8ea1e014b7b60b994
Sergey-8057 <erste_sergey@proton.me>
2025-07-19 02:52:38 +0300

    completing tasks

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import toast, { Toaster } from 'react-hot-toast';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { fetchNotes } from '../../services/noteService.ts';
+import ErrorMessage from '../ErrorMessage/ErrorMessage.tsx';
+import Loader from '../Loader/Loader.tsx';
+import Modal from '../Modal/Modal.tsx';
+import NoteList from '../NoteList/NoteList.tsx';
+import NoteForm from '../NoteForm/NoteForm.tsx';
+import Pagination from '../Pagination/Pagination.tsx';
+import SearchBox from '../SearchBox/SearchBox.tsx';
+
+import css from './App.module.css';
+
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isModalNote, setIsModalNote] = useState(false);
+  const perPage = 12;
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryKey: ['notes', query, page],
+    queryFn: () => fetchNotes(query, page, perPage),
+    placeholderData: keepPreviousData,
+  });
+
+  useEffect(() => {
+    if (isSuccess && data.notes.length === 0) {
+      toast.error('No notes found for your request.');
+    }
+  }, [isSuccess, data]);
+
+  const totalPages = data?.totalPages ?? 0;
+  
+  const updateQuery = useDebouncedCallback((newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
+  }, 1000);
+
+  const openModal = () => setIsModalNote(true);
+  const closeModal = () => setIsModalNote(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className={css.app}>
+      <Toaster position="top-center" reverseOrder={false} />
+      <header className={css.toolbar}>
+        <SearchBox value={query} onSearch={updateQuery} />
+        {isSuccess && totalPages > 1 && (
+          <Pagination page={page} totalPages={totalPages} changePage={setPage} />
+        )}
+        <button className={css.button} onClick={openModal}>
+          Create note +
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {isSuccess && data.notes.length > 0 && <NoteList notes={data.notes} />}
+      {isModalNote && (
+        <Modal onClose={closeModal}>
+          <NoteForm onClose={closeModal} />
+        </Modal>
+      )}
+    </div>
+  );
 }
-
-export default App
---
+++ src/types/note.ts
@@ -0,0 +1,12 @@
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export interface NewNoteData {
+  title: string;
+  content: string;
+  tag: string;
+}
